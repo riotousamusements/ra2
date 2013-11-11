f1aac9ed56302841cf9ff9e6d322363a464f7b76
Riotous Amusements <riotousamusements@gmail.com>
2013-11-10 19:56:18 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Riotousamusements::Application.config.secret_key_base = '6e046388c7f652e7a466c18de9f8dd8145e104577bd08f4f932f11a8ad7f5ecc2adcda992dcb75f3460357f21cd9f73476817a4ef1a9db7715e0a0ed2bef838e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Riotousamusements::Application.config.session_store :cookie_store, key: '_riotousamusements_session'
