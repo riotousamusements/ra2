21f89ec3e362a1fabd795bb214a43e65402ef6ae
Riotous Amusements <riotousamusements@gmail.com>
2013-12-12 15:00:51 -0500

    back to basics

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,10 +1,10 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, [:show, :edit, :update, :destroy]
 
   def index
-    @pins = Pin.all
+    @pins = Pin.all.order("created_at DESC")
   end
 
   def show
@@ -20,7 +20,7 @@ class PinsController < ApplicationController
   def create
     @pin = current_user.pins.build(pin_params)
     if @pin.save
-      redirect_to @pin, notice: 'Pin was successfully created.'
+      redirect_to @pin, notice: 'You successfully created your Marble.'
     else
       render action: 'new'
     end
@@ -28,7 +28,7 @@ class PinsController < ApplicationController
 
   def update
     if @pin.update(pin_params)
-      redirect_to @pin, notice: 'Pin was successfully updated.'
+      redirect_to @pin, notice: 'You successfully updated your Marble.'
     else
       render action: 'edit'
     end
@@ -47,11 +47,11 @@ class PinsController < ApplicationController
 
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+      redirect_to pins_path, notice: "Not authorized to edit this Marble." if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description, :image)
+      params.require(:pin).permit(:description, :image)  
     end
-end
+  end
