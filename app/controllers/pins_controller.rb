ab3c0dcd94efcdb42b1f2588da996f9c9903e545
Riotous Amusements <riotousamusements@gmail.com>
2013-12-12 14:57:38 -0500

    pin controller change to see if aws will work

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
@@ -20,7 +20,7 @@ class PinsController < ApplicationController
   def create
     @pin = current_user.pins.build(pin_params)
     if @pin.save
-      redirect_to @pin, notice: 'You successfully created your Marble.'
+      redirect_to @pin, notice: 'Pin was successfully created.'
     else
       render action: 'new'
     end
@@ -28,7 +28,7 @@ class PinsController < ApplicationController
 
   def update
     if @pin.update(pin_params)
-      redirect_to @pin, notice: 'You successfully updated your Marble.'
+      redirect_to @pin, notice: 'Pin was successfully updated.'
     else
       render action: 'edit'
     end
@@ -47,11 +47,11 @@ class PinsController < ApplicationController
 
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path, notice: "Not authorized to edit this Marble." if @pin.nil?
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description, :image)  
+      params.require(:pin).permit(:description, :image)
     end
 end
