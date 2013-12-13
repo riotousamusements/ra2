c8c8d627bdaaacd28150ccf66237caad6f93747d
Riotous Amusements <riotousamusements@gmail.com>
2013-12-13 06:07:42 -0500

    some style overhaul and contact page generated and placeheld

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Riotousamusements::Application.routes.draw do
+  get "pages/contact"
   resources :pins
 
   devise_for :users
   root "pages#home"
   get "about" => "pages#about"
+  get "contact" => "pages#contact"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
